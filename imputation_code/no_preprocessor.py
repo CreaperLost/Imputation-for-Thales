6ed66fe705fe0de3d57313ec97e8cdef73734ea2
George Paterakis <creaperlost@gmail.com>
2023-11-07 16:20:59 +0200

    First Preprocessing Jobs.

---
+++ imputation_code/no_preprocessor.py
@@ -0,0 +1,42 @@
+from autosklearn.pipeline.components.base import AutoSklearnPreprocessingAlgorithm
+from autosklearn.pipeline.constants import SPARSE, DENSE, UNSIGNED_DATA, INPUT
+from autosklearn.askl_typing import FEAT_TYPE_TYPE
+from ConfigSpace.configuration_space import ConfigurationSpace
+from typing import Optional
+from pprint import pprint
+import autosklearn.classification
+
+
+class NoPreprocessing(AutoSklearnPreprocessingAlgorithm):
+    def __init__(self, **kwargs):
+        """This preprocessors does not change the data"""
+        # Some internal checks makes sure parameters are set
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+
+    def fit(self, X, Y=None):
+        return self
+
+    def transform(self, X):
+        return X
+
+    @staticmethod
+    def get_properties(dataset_properties=None):
+        return {
+            "shortname": "NoPreprocessing",
+            "name": "NoPreprocessing",
+            "handles_regression": True,
+            "handles_classification": True,
+            "handles_multiclass": True,
+            "handles_multilabel": True,
+            "handles_multioutput": True,
+            "is_deterministic": True,
+            "input": (SPARSE, DENSE, UNSIGNED_DATA),
+            "output": (INPUT,),
+        }
+
+    @staticmethod
+    def get_hyperparameter_search_space(
+        feat_type: Optional[FEAT_TYPE_TYPE] = None, dataset_properties=None
+    ):
+        return ConfigurationSpace()  # Return an empty configuration as there is None
