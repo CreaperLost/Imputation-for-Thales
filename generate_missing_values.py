71994a092a8746f7935bb74e8010992c9c84d5c0
George Paterakis <creaperlost@gmail.com>
2023-11-12 21:32:15 +0200

    requirements txt

--- generate_missing_values.py
+++ generate_missing_values.py
@@ -20,3 +20,30 @@ def generate_mcar_missing_values(data: pd.DataFrame, percentage: float) -> pd.Da
     
     return new_data
 
+breast_data = pd.read_csv('datasets/breast-w.csv')
+Diabetes_data = pd.read_csv('datasets/Diabetes.csv')
+
+
+breast_data.head(5)
+breast_data.drop(columns=['subspaces'],inplace=True)
+breast_data
+
+
+Diabetes_data.head(5)
+Diabetes_data.drop(columns=['subspaces'],inplace=True)
+Diabetes_data
+
+#Insert missing values to both datasets!
+
+# Find the feature's names.
+cardio_cols = [i for i in list(Diabetes_data.columns) if i != 'is_anomaly']
+breast_cols = [i for i in list(breast_data.columns) if i != 'is_anomaly']
+
+#Only inser missing data at features!
+Diabetes_data.loc[:,cardio_cols] = generate_mcar_missing_values(Diabetes_data.loc[:,cardio_cols], 0.1).copy()
+
+breast_data.loc[:,breast_cols] = generate_mcar_missing_values(breast_data.loc[:,breast_cols], 0.1).copy()
+
+
+Diabetes_data.to_csv('missing_data/diabetes.csv',index=None)
+breast_data.to_csv('missing_data/breast.csv',index=None)
--- imputation_test_run.py
+++ imputation_test_run.py
@@ -7,30 +7,8 @@ from generate_missing_values import generate_mcar_missing_values
 from run_autosklearn import AutoML
 
 
-
 breast_data = pd.read_csv('datasets/breast-w.csv')
 Diabetes_data = pd.read_csv('datasets/Diabetes.csv')
-
-
-breast_data.head(5)
-breast_data.drop(columns=['subspaces'],inplace=True)
-breast_data
-
-
-Diabetes_data.head(5)
-Diabetes_data.drop(columns=['subspaces'],inplace=True)
-Diabetes_data
-
-#Insert missing values to both datasets!
-
-# Find the feature's names.
-cardio_cols = [i for i in list(Diabetes_data.columns) if i != 'is_anomaly']
-breast_cols = [i for i in list(breast_data.columns) if i != 'is_anomaly']
-
-#Only inser missing data at features!
-Diabetes_data.loc[:,cardio_cols] = generate_mcar_missing_values(Diabetes_data.loc[:,cardio_cols], 0.1).copy()
-
-breast_data.loc[:,breast_cols] = generate_mcar_missing_values(breast_data.loc[:,breast_cols], 0.1).copy()
 
 
 print(Diabetes_data['is_anomaly'].value_counts())
