e18cd82b0d70045e80d6bb9b99f5d8f2565e86d7
George Paterakis <creaperlost@gmail.com>
2023-11-12 22:07:06 +0200

    Lovely.

--- imputation_test_run.py
+++ imputation_test_run.py
@@ -7,27 +7,29 @@ from generate_missing_values import generate_mcar_missing_values
 from run_autosklearn import AutoML
 
 
-breast_data = pd.read_csv('missing_datasets/breast.csv')
-Diabetes_data = pd.read_csv('missing_datasets/diabetes.csv')
+breast_data = pd.read_csv('missing_data/breast.csv')
+Diabetes_data = pd.read_csv('missing_data/diabetes.csv')
 
 
 # Find the feature's names.
 cardio_cols = [i for i in list(Diabetes_data.columns) if i != 'is_anomaly']
 breast_cols = [i for i in list(breast_data.columns) if i != 'is_anomaly']
 
+train_budget_minutes = 120
+
 print('Running the diabetes data')
-AutoML(Diabetes_data.loc[:,cardio_cols].to_numpy(), Diabetes_data.loc[:,'is_anomaly'].to_numpy(),5)
+imputed_diabetes = AutoML(Diabetes_data.loc[:,cardio_cols].to_numpy(), Diabetes_data.loc[:,'is_anomaly'].to_numpy(),train_budget_minutes)
 
 
 print('Running the breast data')
 
 
-AutoML(breast_data.loc[:,breast_cols].to_numpy(), breast_data.loc[:,'is_anomaly'].to_numpy(),5)
+imputed_breast= AutoML(breast_data.loc[:,breast_cols].to_numpy(), breast_data.loc[:,'is_anomaly'].to_numpy(),train_budget_minutes)
 
 
+imputed_diabetes.to_csv('imputed_data/diabetes.csv',index=None)
 
-
-
+imputed_breast.to_csv('imputed_data/breast.csv',index=None)
 
 
 
